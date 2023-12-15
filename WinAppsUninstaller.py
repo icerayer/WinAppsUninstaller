6719b06bf7636819aad99254d0f223d9e487404f
icerayer <56902206+icerayer@users.noreply.github.com>
2023-12-15 14:08:36 +0800

    增加第三方分类Developer,补充注释

--- WinAppsUninstaller.py
+++ WinAppsUninstaller.py
@@ -3,8 +3,8 @@ import PySimpleGUI as sg
 import pyperclip
 
 
-# 版本变更：0.8.1
-# 修改tidy()，绿色放前面不变，黑色放中间，紫色放到最后
+# 版本变更：0.8.2
+# 增加第三方分类Developer
 
 # appinfdic的key有：["Name", "Publisher", "Architecture", "ResourceId", "Version","PackageFullName", "InstallLocation", "IsFramework", "PackageFamilyName","PublisherId", "IsResourcePackage", "IsBundle", "IsDevelopmentMode","NonRemovable", "IsPartiallyStaged", "SignatureKind", "Status", 'Dependencies']
 def uninstall_selected_apps(sel_apps):
@@ -23,7 +23,7 @@ def uninstall_selected_apps(sel_apps):
                     print(result.stdout)
                     print(result.stderr)
                 if notfoundAppx(app):
-                    print(app, '确认不存在')
+                    print(app, '确认已卸载!')
                     window[app].update(text_color='red')
                 else:
                     print('应用还存在，似乎未卸载成功')
@@ -32,15 +32,19 @@ def uninstall_selected_apps(sel_apps):
         print('\n')
 
 
-def notfoundAppx(app_name):
+def notfoundAppx(app_name):  # 检查指定的应用程序是否不存在
+    # 定义一个命令，用于搜索指定应用程序
     command = f"Get-AppxPackage -Name '*{app_name}*'"
     try:
+        # 使用PowerShell运行命令，并将输出捕获到check_result中
         check_result = subprocess.run(["powershell", command], capture_output=True, text=True)
+        # 如果输出为空，则表示应用程序不存在
         if check_result.stdout.strip() == "":
             return True
         else:
             return False
     except Exception as e:
+        # 如果出现异常，则打印异常信息
         print(f"An error occurred: {e}")
 
 
@@ -49,54 +53,67 @@ def getColor(curr_name):
         return 'purple'
     elif curr_name in recom_dict.keys():
         return 'green'
+    elif curr_name in dev_list:
+        return 'blue'
     else:
         return 'black'
 
 
-def getName(name):
+def getName(name):  # 获取推荐列表中的名字
+    # 判断name是否在recom_dict中
     if name in recom_dict.keys():
+        # 如果在，返回recom_dict中的值
         return recom_dict[name]
     else:
+        # 如果不在，返回name
         return name
 
 
 def tidy(name_list):
-    top = []
-    tail = []
-    mid = []
+    recom = []
+    noml = []
+    dev = []
+    warn = []
+
     for x in name_list:
         if x in recom_dict.keys():
-            top.append(x)
+            recom.append(x)
         elif x in warn_list:
-            tail.append(x)
+            warn.append(x)
+        elif x in dev_list:
+            dev.append(x)
         else:
-            mid.append(x)
-    top.sort()
-    tail.sort()
-    print('推荐卸载', len(top))
-    return top + mid + tail
+            noml.append(x)
+    recom.sort()
+    noml.sort()
+    dev.sort()
+    warn.sort()
+    print('推荐卸载\t', len(recom))
+    print('第三方\t', len(dev))
+    print('不建议卸载\t', len(warn))
+    return recom + noml + dev + warn
 
 
 def create_layout():
     layout = []
-    col_num = 4
+    col_num = 4  # 列数为4，一行显示4个
     row_num, remainder = divmod(len(name_list), col_num)
-    if remainder:
+    if remainder:  # 如果还有剩余，则行数加1
         row_num += 1
-    start = 0
-    for row in range(row_num):
+    start = 0  # 每行起始元素在name_list中的位置索引
+    for row in range(row_num):  # 遍历行
         row_lay = []
-        if remainder and row == row_num - 1:
-            col_num = remainder
-        for flag in range(col_num):
-            curr_name = name_list[start + flag]
-            unit_lay = [
+        if remainder and row == row_num - 1:  # 如果是最后一行
+            col_num = remainder  # 列数等于取模剩余的个数
+        for flag in range(col_num):  # 遍历每行中的元素位置索引
+            curr_name = name_list[start + flag]  # 根据当前元素在name_list中的位置索引(起始位索引+行内索引)获取应用名
+            unit_lay = [  # 绘制每个应用的布局
                 sg.Checkbox(getName(curr_name), size=(30, 1), key=curr_name, text_color=getColor(curr_name)),
                 sg.Button('卸载', key=f'button-{curr_name}')
             ]
-            row_lay.append(sg.Column([unit_lay]))
-        layout.append(row_lay)
-        start += col_num
+            row_lay.append(sg.Column([unit_lay]))  # 将每个应用的布局添加进row布局
+        layout.append(row_lay)  # 把每行添加进layout
+        start += col_num  # 每行起始元素在name_list中的位置索引+列数，等于下一行首位的索引
 
     layout.append([
         sg.Button('全选'),
@@ -105,6 +122,7 @@ def create_layout():
         sg.Button('一键卸载'),
         sg.Button('退出'),
         sg.Text('推荐卸载', text_color='green'),
+        sg.Text('第三方', text_color='blue'),
         sg.Text('不推荐卸载', text_color='purple'),
         sg.Text('已卸载', text_color='red'),
     ])
@@ -164,6 +182,7 @@ echo_list = list(filter(None, echo.split("\r\n\r\n")))
 # 创建一个空列表，用于存储包的信息
 pkg_list = []  # 包列表
 name_list = []
+dev_list = []
 # 遍历echo_list中的每个元素（即每个包的信息）
 for echo_item in echo_list:
     # 将字符串按照换行符进行分割，得到一个列表
@@ -182,6 +201,8 @@ for echo_item in echo_list:
     if appinfdic.get("NonRemovable") == "False" and appinfdic.get("IsFramework") == "False":
         pkg_list.append(appinfdic)  # 将一个包字典存入包列表
         name_list.append(appinfdic.get('Name'))
+    if appinfdic.get("SignatureKind") == "Developer":
+        dev_list.append(appinfdic.get('Name'))
 
 name_list = tidy(name_list)  # 推荐列表中的应用置顶
 print("可卸载的非框架应用", len(name_list))
